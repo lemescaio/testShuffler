94608def924227f2a5c57fa891a7075c78d6e9e0
Caio Lemes <lemes_caio@hotmail.com>
2022-03-10 21:35:34 -0300

    add loop aplic. e comentarios

--- embaralhaProvas.py
+++ embaralhaProvas.py
@@ -5,18 +5,22 @@ import os
 
 
 def buscaEstados(dic):
+    '''retorna uma lista dos estados do brasil'''
     return list(dic.keys())
 
 
 def buscaCapitais(dic):
+    '''retorna uma lista com as capitais do brasil'''
     return list(dic.values())
 
 
 def completaEnunciado(numQ, uf):
+    '''formata string do enunciado das questoes'''
     return '{0}. Qual é a capital do(e) {1}?\n'.format(numQ, uf)
 
 
 def completaRespostas(letras, listaRespQuestao):
+    '''formata string das respostas das questoes'''
     valorResp = ''
     i = 0
     for resp in listaRespQuestao:
@@ -26,10 +30,12 @@ def completaRespostas(letras, listaRespQuestao):
     return valorResp
 
 def completaCabecalho(nCab):
+    '''formata string do cabecalho da prova'''
     return 'Nome:\n\nData:\n\nPeríodo:\n\n\t\t\tQuiz - Estados e Capitais (Versão {0})\n\n'.format(nCab)
 
 
 def completaGabarito(respCorr, letras, listaRespQuestao, nQue):
+    '''formata string do gabarito da prova'''
     indexL = listaRespQuestao.index(respCorr)
     letraCerta = letras[indexL]
     gabarito = str(nQue) + '. ' + letraCerta + '\n'
@@ -37,6 +43,7 @@ def completaGabarito(respCorr, letras, listaRespQuestao, nQue):
 
 
 def criaPasta(pasta):
+    '''cria pastas para salvar arquivos ou deleta os arquivos existentes nelas'''
     if os.path.exists(pasta):
         for fileName in os.listdir(pasta):
             os.unlink(os.path.join(pasta, fileName))
@@ -45,20 +52,11 @@ def criaPasta(pasta):
 
 
 def gravaArquivo(pasta, arq, info):
+    '''salva arquivos no formato txt'''
     with open(os.path.join(pasta, arq), 'w') as fileArq:
         fileArq.write(info)
 
-
-strNumQuestoes = 3
-strNumRespostas = 5
-strNumProvas = 4
-
-numQuestoes = 3
-numRespostas = 5
-numProvas = 4
-
-letras = list(string.ascii_uppercase[:numRespostas])
-
+# lista com todos Estados e Capitais
 capitaisEstados = { 'Acre (AC)': 'Rio Branco',
                     'Amapá (AP)': 'Macapá',
                     'Amazonas (AM)': 'Manaus',
@@ -87,46 +85,92 @@ capitaisEstados = { 'Acre (AC)': 'Rio Branco',
                     'Santa Catarina (SC)': 'Florianópolis',
                     'Rio Grande do Sul (RS)': 'Porto Alegre'}
 
-camGabarito = './Gabarito'
-camProva = './Prova'
+# variavel para iniciar a aplicação
+iProg = '1'
+while iProg == '1': # loop para rodar aplicação
+    iProg = input('Digite 1 para geras as versões das provas ou qualquer tecla para sair: ')
+    if iProg == '1':
+        camGabarito = './Gabarito' # caminho para salvar arquivos do Gabarito
+        camProva = './Prova' # caminho para salvar arquivos da Prova
 
-criaPasta(camGabarito)
-criaPasta(camProva)
+        # input para usuario digitar informações
+        strNumQuestoes = input('Digite quantas questões terá na prova: ')
+        strNumRespostas = input('Digite quantas respostas terá cada questão: ')
+        strNumProvas = input('Digite quantas versões terá de cada prova: ')
 
-perguntasDefinitivo = []
-estados = buscaEstados(capitaisEstados)
-estadosSelecionados = random.sample(estados, k=numQuestoes)
+        # validação das informações digitadas
+        if strNumQuestoes.isnumeric():
+            numQuestoes = int(strNumQuestoes)
+        else:
+            print('Valor ínválido, digite um número inteiro.')
+            break
 
-for estado in estadosSelecionados:
-    respostaCorreta = capitaisEstados[estado]
-    capitais = buscaCapitais(capitaisEstados)
-    capitais.remove(respostaCorreta)
-    respostasLista = random.sample(capitais, k=numRespostas - 1)
-    respostasLista.append(respostaCorreta)
+        if strNumRespostas.isnumeric():
+            numRespostas = int(strNumRespostas)
+        else:
+            print('Valor ínválido, digite um número inteiro.')
+            break
 
-    perguntasParcial = []
-    perguntasParcial.append(estado)
-    perguntasParcial.append(respostasLista)
+        if strNumProvas.isnumeric():
+            numProvas = int(strNumProvas)
+        else:
+            print('Valor ínválido, digite um número inteiro.')
+            break
 
-    perguntasDefinitivo.append(perguntasParcial)
+        # cria caminho ou deleta arquivos dentro do diretorio
+        criaPasta(camGabarito)
+        criaPasta(camProva)
 
-nP = 1
-for nProvas in range(numProvas):
-    nQ = 1
-    versaoProva = random.sample(perguntasDefinitivo, k=len(perguntasDefinitivo))
-    strVersao = completaCabecalho(nP)
-    strGabarito = ''
-    for nQuestoes  in versaoProva:
-        strQuestao = completaEnunciado(nQ, nQuestoes[0])
-        listaRespQuestao = []
-        listaRespQuestao = random.sample(nQuestoes[1], k=len(nQuestoes[1]))
-        strResposta = completaRespostas(letras, listaRespQuestao)
-        strVersao += strQuestao + strResposta
+        # lista com sequencia de letras
+        letras = list(string.ascii_uppercase[:numRespostas])
 
-        respostaCorreta = capitaisEstados[nQuestoes[0]]
-        strGabarito += completaGabarito(respostaCorreta, letras, listaRespQuestao, nQ)
-        nQ += 1
-    
-    gravaArquivo(camProva, 'Prova_versao_' + str(nP) + '.txt', strVersao)
-    gravaArquivo(camGabarito, 'Gabarito_versao_' + str(nP) + '.txt', strGabarito)
-    nP += 1
+        perguntasDefinitivo = [] # instancia lista de perguntas
+        estados = buscaEstados(capitaisEstados) # carrega lista com estados 
+        estadosSelecionados = random.sample(estados, k=numQuestoes) # random dos estados conforme 
+        # preenchido pelo usuário
+
+        for estado in estadosSelecionados: # loop nos estados selecionados
+            respostaCorreta = capitaisEstados[estado] # carrega resposta correta
+            capitais = buscaCapitais(capitaisEstados) # carrega lista com capitais
+            capitais.remove(respostaCorreta) # remove resposta correta da lista de capitais
+            respostasLista = random.sample(capitais, k=numRespostas - 1) # randon das capitais
+            # conforma preenchido pelo usuário
+            respostasLista.append(respostaCorreta) # retonar resposta correta na lista de capitais
+
+            perguntasParcial = [] # instancia variavel de perguntas parcial
+            perguntasParcial.append(estado) # adiciona estado na lista parcial
+            perguntasParcial.append(respostasLista) # adiciona respostas na lista parcial
+
+            perguntasDefinitivo.append(perguntasParcial) # adiciona lista parcial na definitiva
+
+        nP = 1 # contador numero de provas
+        for nProvas in range(numProvas): # loop nas versões de provas
+            nQ = 1 # contador numero de questões
+            versaoProva = random.sample(perguntasDefinitivo, k=len(perguntasDefinitivo)) # altera
+            # sequencia da ordens das questoes
+            strVersao = completaCabecalho(nP) # carrega padrao de cabeçalho
+            strGabarito = '' # instancia string gabarito
+            for nQuestoes  in versaoProva: # loop nas perguntas da versão vigente
+                strQuestao = completaEnunciado(nQ, nQuestoes[0]) # carrega padrão enunciado
+                listaRespQuestao = [] # instancia Lista de resposta da questao
+                listaRespQuestao = random.sample(nQuestoes[1], k=len(nQuestoes[1])) # altera ordem
+                # das respostas da questão
+                strResposta = completaRespostas(letras, listaRespQuestao) # carrega padrao de resposta
+                strVersao += strQuestao + strResposta # junta questao e reposta na string
+
+                respostaCorreta = capitaisEstados[nQuestoes[0]] # carrega resposta correta da questao
+                strGabarito += completaGabarito(respostaCorreta, letras, listaRespQuestao, nQ)
+                # carrega padrão gabarito
+                nQ += 1
+            
+            gravaArquivo(camProva, 'Prova_versao_' + str(nP) + '.txt', strVersao)
+            # grava arquivos prova
+            gravaArquivo(camGabarito, 'Gabarito_versao_' + str(nP) + '.txt', strGabarito)
+            # grava arquivos gabarito
+            nP += 1
+        
+        print('Arquivos gerados.')
+
+    else:
+        print('Até mais!')
+        break
